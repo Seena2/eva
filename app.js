288475e97b3f1b349db2cc0b585b7358b88e7d85
Seena2 <dinku22@gmail.com>
2023-08-11 00:34:52 +0300

    css cpd page started

--- app.js
+++ app.js
@@ -79,7 +79,29 @@ app.get('/contact', (req, res) => {
 app.get('/news/create', (req, res) => {
     res.render('create', { title: "Create News" });
 });
+
 //Other Links within the pages
+//EVJ
+app.get('/archive', (req, res) => {
+    res.render('archive', { title: "archive" });
+});
+app.get('/evj-vol27n2', (req, res) => {
+    res.render('evj-vol27n2', { title: "evj-vol27n2" });
+});
+app.get('/evj-vol27n1', (req, res) => {
+    res.render('evj-vol27n1', { title: "evj-vol27n1" });
+});
+app.get('/evj-vol26n2', (req, res) => {
+    res.render('evj-vol26n2', { title: "evj-vol26n2" });
+});
+app.get('/evj-vol26n1', (req, res) => {
+    res.render('evj-vol26n1', { title: "evj-vol26n1" });
+});
+app.get('/evj-vol25n2', (req, res) => {
+    res.render('evj-vol25n2', { title: "evj-vol25n2" });
+});
+
+//TRaining
 app.get('/courses', (req, res) => {
     res.render('courses', { title: "courses" });
 });
